ee4d8085a6193b0d71e1a5e5ab523b6a15d3d200
Вовк Павло <grugysvovks1980@gmail.com>
2025-11-30 15:57:45 +0200

    feat: Read names from data/names.txt

--- greetings.py
+++ greetings.py
@@ -1,7 +1,12 @@
 import datetime
 
-name = input("Введіть ваше ім'я: ")
-
+# Рядок 4
+with open('data/names.txt', 'r', encoding='utf-8') as f:
+# Рядок 5: ПЕРЕВІРТЕ: Тут мають бути 4 пробіли
+    names = [line.strip() for line in f]
+    
 current_time = datetime.datetime.now().strftime("%H:%M:%S")
 
-print(f"Привіт, {name}! Поточний час: {current_time}")
+for name in names:
+# Рядок 9: ПЕРЕВІРТЕ: Тут мають бути 4 пробіли
+    print(f"Привіт, {name}! Поточний час: {current_time}")    
